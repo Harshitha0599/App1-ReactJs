4cbf5b2526557cc239ec363aaa4cfc52e6ad6c0d
Harshitha0599 <harshithabodhu09@gmal.com>
2026-05-01 09:29:46 +0530

    config:modified

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,12 +1,12 @@
-function Home({ name }) {
-    return (
-        // <h1>Home </h1>
-        <>
-         <h1>Home </h1>
-        <h1>Welcome {name} </h1>
-        </>
-    )
-}
+// function Home({ name }) {
+//     return (
+//         // <h1>Home </h1>
+//         <>
+//          <h1>Home </h1>
+//         <h1>Welcome {name} </h1>
+//         </>
+//     )
+// }
 
 export default Home
 
@@ -18,3 +18,36 @@ export default Home
 //             </>
 //         )
 // }
+
+// function Home () {
+//     return (
+//         <>
+//         <h1>useState-Hook</h1>
+//         <h3>{count}</h3>
+//         <button onClick ={incCount}>Increment</button>
+//         </>
+//     )
+// }
+
+
+
+import { useState } from 'react'
+function Home () {
+  const [count,setCount] = useState(0)
+
+  const incCount = () => {
+    // setCount (count + 1);
+    // setCount (count + 1);
+    // setCount (count + 1); 
+    setCount ((prevcount) => prevcount + 1);
+    setCount ((prevcount) => prevcount + 1);
+    setCount ((prevcount) => prevcount + 1);
+  }
+  return (
+        <>
+        <h1>useState-Hook</h1>
+        <h3>{count}</h3>
+        <button onClick ={incCount}>Increment</button>
+        </>
+    )
+}
